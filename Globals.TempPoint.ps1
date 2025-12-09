282485bd80d26bdc67ba9bf474632603f8492e6a
chida <christian@damberg.org>
2025-12-09 07:54:34 +0100

    Update

--- Globals.TempPoint.ps1
+++ Globals.TempPoint.ps1
@@ -382,7 +382,7 @@ function Copy-WithProgress
 	$ProgressBarBackup.Step = 1
 	$ProgressBarBackup.Value = 0
 	
-	$CommonRobocopyParams = '/MIR /NP /MT:24 /NDL /NC /BYTES /NJH /NJS /xf *.log'
+	$CommonRobocopyParams = '/MIR /NP /NDL /NC /BYTES /NJH /NJS /xf *.log'
 	
 	$FilebackupWindow.AppendText("`nAnalyzing robocopy job ...")
 	
